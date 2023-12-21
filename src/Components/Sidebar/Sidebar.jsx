4f4f481fad274c16770e62803088b99ffb3ecaf9
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-12-22 04:26:15 +0600

    added dashboard fucntionality

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="drawer ">
+    <div className="drawer inline-block">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         {/* Page content here */}
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import MainRoutes from "./Routes/MainRoutes.jsx";
 import AuthProviders from "./Providers/AuthProvider.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProviders>
-      <RouterProvider router={MainRoutes}></RouterProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={MainRoutes}></RouterProvider>
+      </QueryClientProvider>
     </AuthProviders>
   </React.StrictMode>
 );
