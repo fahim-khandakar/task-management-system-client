7eb3ae9efb9b4d73e58c32af117b3adf89759844
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-11-26 00:31:37 +0600

    some change

--- src/Components/Todo Archived/TodoArchived.jsx
+++ src/Components/Todo Archived/TodoArchived.jsx
@@ -1,6 +1,7 @@
 import ArchivedTodo from "../../Hooks/ArchivedTodo";
 import LoadingPage from "../Loading Page/LoadingPage";
 import ArchivedTaskCard from "./partials/Archived Card/ArchivedCard";
+import noData from "../../assets/no-data.jpg";
 
 const TodoArchived = () => {
   const { archivedTodo, isLoading } = ArchivedTodo();
@@ -10,11 +11,17 @@ const TodoArchived = () => {
   }
 
   return (
-    <div>
-      <div className="grid grid-cols-4 gap-3 px-20">
-        {archivedTodo?.map((todo, index) => (
-          <ArchivedTaskCard key={index} task={todo} />
-        ))}
+    <div className="w-full">
+      <div className="grid grid-cols-4 gap-3 px-20 w-full">
+        {archivedTodo?.length ? (
+          archivedTodo?.map((todo, index) => (
+            <ArchivedTaskCard key={index} task={todo} />
+          ))
+        ) : (
+          <div className=" col-span-4">
+            <img className="w-1/4 mx-auto h-auto" src={noData} alt="" />
+          </div>
+        )}
       </div>
     </div>
   );
